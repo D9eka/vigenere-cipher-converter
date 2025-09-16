3a75bc6de93578040bd8a97eb33f0e920565e419
D9eka <120364200+D9eka@users.noreply.github.com>
2025-09-16 13:59:33 +0500

    Исправление InputValidationResult Validate

--- Lab2/Services/Input/InputValidator.cs
+++ Lab2/Services/Input/InputValidator.cs
@@ -8,7 +8,7 @@ public class InputValidator
     public InputValidationResult Validate(string input, Alphabet alphabet)
     {
         if (string.IsNullOrWhiteSpace(input))
-            return InputValidationResult.Error("Введите текст для обработки.");
+            return InputValidationResult.Error("Введите текст для обработки");
 
         int validChars = input.Count(c =>
             c >= (char)alphabet.StartCharIndex && c <= (char)alphabet.EndCharIndex
@@ -21,7 +21,7 @@ public class InputValidator
             c >= (char)alphabet.StartCharIndex && c <= (char)alphabet.EndCharIndex
             || alphabet.CharsToReplace.ContainsKey(c) || c == ' ');
 
-        if (validCharsWithSpace < validChars || validChars < input.Length)
+        if (validCharsWithSpace < input.Length)
             return InputValidationResult.Warning("Некоторые символы не соответствуют алфавиту и будут пропущены");
 
         return InputValidationResult.Success();
